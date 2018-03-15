30cf16c1f8a1742a5b8f18875f5f4b523b9a7647
Arjan <bernie592>
2018-03-15 10:05:48 +0100

    Initial commit

---
+++ src/main/java/Arjan580455/App.java
@@ -0,0 +1,13 @@
+package Arjan580455;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
